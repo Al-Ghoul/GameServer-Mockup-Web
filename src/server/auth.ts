4f539bfafa5c582a130ca5f00165dfd27a7ede86
Al-Ghoul <Abdo.alghouul@gmail.com>
2023-03-05 11:31:02 +0200

    Add Custom signIn/error pages paths

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -90,6 +90,10 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  pages: {
+    signIn: '/auth/login',
+    error: '/auth/error',
+  },
   session: {
     strategy: "jwt"
   },
