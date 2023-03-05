97161afdee83a751954e9d5e222567d5c5dbedea
Al-Ghoul <Abdo.alghouul@gmail.com>
2023-03-05 09:35:51 +0200

    Move consistently reused layout tp  persist single/similar layout between pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,10 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
+import { Footer } from "~/components/Footer";
+import { Header } from "~/components/Header";
+
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +16,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>GameServer Mockup</title>
+        <meta name="description" content="A mock-up that connects to a native c++ websocket server." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </SessionProvider>
   );
 };
