87694acf7d9be0a9597f4eb2c096470815d753aa
Al-Ghoul <Abdo.alghouul@gmail.com>
2023-03-07 13:06:55 +0200

    Final version, I lose the track of these files.

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,36 +3,74 @@ import { TRPCClientError } from "@trpc/client";
 
 import {
   createTRPCRouter,
-  publicProcedure,
   protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
-import { UserRegistrationInput } from "~/utils/UserTypes";
+import { SetNameInput, UserRegistrationInput } from "~/utils/UserTypes";
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
 
 export const userRouter = createTRPCRouter({
   register: publicProcedure
     .input(UserRegistrationInput)
     .mutation(async ({ input, ctx }) => {
-      const { name, password, email } = input;
+      const { username, password } = input;
+      const SALT_ROUNDS = 10;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call,  @typescript-eslint/no-unsafe-assignment
+      const new_password: string = await bcrypt.hash(password, SALT_ROUNDS);
 
       try {
         await ctx.prisma.user.create({
           data: {
-            name, password, email
+            username, password: new_password
           }
         });
       } catch (error) {
         if (error instanceof Prisma.PrismaClientInitializationError) {
-          throw new TRPCClientError(`[{\"message\": \"Database error, please report this issue or try again later.\"}]`);
+          throw new TRPCClientError("Database error, please report this issue or try again later");
         }
 
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-          if (error.code === 'P2002') throw new TRPCClientError('[{\"message\": \"User already exists.\"}]');
+          if (error.code === 'P2002') throw new TRPCClientError("User already exists.");
         }
       }
 
       return {
         status: "Success",
-        message: `${name} was created successfully`,
+        message: `${username} was created successfully`,
+      };
+    }),
+  setUsername: protectedProcedure
+    .input(SetNameInput)
+    .mutation(async ({ input, ctx }) => {
+
+      try {
+        await ctx.prisma.user.update({
+          where: {
+            id: ctx.session.user.id
+          },
+          data: {
+            ...input
+          }
+        });
+      } catch (error) {
+        if (error instanceof Prisma.PrismaClientInitializationError) {
+          throw new TRPCClientError("Database error, please report this issue or try again later");
+        }
+
+        // if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        //   if (error.code === 'P2002') throw new TRPCClientError("Sorry! This name is already used, can you come up with another one?");
+        // }
+        // console.log(error)
+        throw new TRPCClientError("Seems like this user was not found");
+      }
+
+      return {
+        status: "Success",
+        message: `${input.name} was set successfully`,
       };
     }),
 });
+
+
+
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -10,6 +10,11 @@ import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 import CredentialsProvider from "next-auth/providers/credentials"
 import { UserLoginInput, type UserLoginInputType } from "~/utils/UserTypes";
+import { Prisma } from "@prisma/client";
+import { type JWT, type JWTDecodeParams, type JWTEncodeParams } from "next-auth/jwt";
+import * as jose from 'jose'
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -39,39 +44,58 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    async jwt({ token, user, account, profile, isNewUser }) {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      if (!token.name && token.id) {
+        const userData = await prisma.user.findFirst({ where: { id: token.id } });
+        if (!!userData) token.name = userData.name;
+      }
       return token;
     },
+    session({ session, token }) {
+      //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      session.user.id = token?.id
+
+      return session
+    }
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: 'Credentials',
-
+      //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      async authorize(credentials: UserLoginInputType, req) {
+      async authorize(credentials: UserLoginInputType) {
 
         const userCredentialsParseResult = UserLoginInput.safeParse(credentials);
         if (!userCredentialsParseResult.success) {
           const formattedErrorMessages = userCredentialsParseResult.error.format();
 
-          if (formattedErrorMessages.name) throw new Error(...formattedErrorMessages.name._errors);
-
-          if (formattedErrorMessages.email) throw new Error(...formattedErrorMessages.email._errors);
+          if (formattedErrorMessages.username) throw new Error(...formattedErrorMessages.username._errors);
 
           if (formattedErrorMessages.password) throw new Error(...formattedErrorMessages.password._errors);
 
           throw new Error(...formattedErrorMessages._errors)
         }
 
+
         let user;
-        if (credentials.email !== "") {
-          user = await prisma.user.findFirst({ where: { email: credentials.email, password: credentials.password } });
-        } else {
-          user = await prisma.user.findFirst({ where: { name: credentials.name, password: credentials.password } });
+        try {
+          user = await prisma.user.findFirst({ where: { username: credentials.username } });
+        } catch (error) {
+          if (error instanceof Prisma.PrismaClientInitializationError) {
+            throw new Error("Database error, please report this issue or try again later");
+          }
         }
 
-        if (user) return user;
+        if (user) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          if (!await bcrypt.compare(credentials.password, user.password))
+            throw new Error("Incorrect password.");
+
+          return user;
+        }
 
         throw new Error("User doesn't exist");
       }
@@ -92,13 +116,31 @@ export const authOptions: NextAuthOptions = {
   ],
   pages: {
     signIn: '/auth/login',
-    error: '/auth/error',
+    newUser: '/auth/new-user'
   },
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   jwt: {
-    maxAge: 60 * 60 * 24 * 30,
+    async encode(params: JWTEncodeParams): Promise<string> {
+
+      return await new jose.SignJWT({ ...params.token })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime('10d')
+        .setIssuer('localhost')
+        .sign(new TextEncoder().encode(params.secret.toString()))
+    },
+    async decode(params: JWTDecodeParams): Promise<JWT | null> {
+      if (params.token) {
+        const { payload } = await jose.jwtVerify(params.token, new TextEncoder().encode(params.secret.toString()), {
+          issuer: 'localhost',
+        })
+
+        return payload;
+      }
+      return null
+    },
   },
 };
 
