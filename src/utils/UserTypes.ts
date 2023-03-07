e6ae92175bb510cdbf74e882c1febe3716c93e71
Al-Ghoul <Abdo.alghouul@gmail.com>
2023-03-07 13:01:00 +0200

    General Refactors & Add SetNameInput

--- src/utils/UserTypes.ts
+++ src/utils/UserTypes.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
 export const UserRegistrationInput = z.object({
-    name: z.string(),
+    username: z.string(),
     password: z.string(),
     confirmPassword: z.string(),
-    email: z.string(),
 }).superRefine((data, ctx) => {
-    if (data.name === "") {
+    if (data.username === "") {
         ctx.addIssue({
             code: z.ZodIssueCode.custom,
-            message: "A name is required",
+            message: "A username is required",
         });
     }
 
@@ -33,56 +32,25 @@ export const UserRegistrationInput = z.object({
             message: "Confirmation password do NOT match.",
         });
     }
-
-    if (data.email !== "") {
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|([^-]([a-zA-Z0-9-]*\.)+[a-zA-Z]{2,}))$/;
-        // @ts-ignore
-        if (!emailRegex.test(data.email)) {
-            ctx.addIssue({
-                code: z.ZodIssueCode.custom,
-                message: "Please provide a valid email",
-            });
-        }
-    } else {
-        ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: "An email is required.",
-        });
-    }
 });
 
 
 export const UserLoginInput = z.object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string()
-}).partial().superRefine((data, ctx) => {
-    if (data.password === "") {
-        ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: "A password MUST be provided",
-        });
-    }
-
-    if (data.email !== "") {
-        const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|([^-]([a-zA-Z0-9-]*\.)+[a-zA-Z]{2,}))$/;
-        // @ts-ignore
-        if (!emailRegex.test(data.email)) {
-            ctx.addIssue({
-                code: z.ZodIssueCode.custom,
-                message: "Please provide a valid email",
-            });
-        }
-    }
-
-    if (data.name === "" && data.email === "") {
-        ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: "Please provide either a name or email",
-        });
-    }
+    username: z.string().refine((val) => val.length > 0, {
+        message: "A username must be provided"
+    }),
+    password: z.string().refine((val) => val.length > 0, {
+        message: "A password must be provided"
+    }),
 }
 );
 
+export const SetNameInput = z.object({
+    name: z.string().refine((val) => val.length > 0, {
+        message: "A name must be provided"
+    }),
+});
+
 export type UserRegistrationInputType = z.infer<typeof UserRegistrationInput>;
 export type UserLoginInputType = z.infer<typeof UserLoginInput>;
+export type SetNameInputType = z.infer<typeof SetNameInput>;
